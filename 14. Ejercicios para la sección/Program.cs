99ff994192e4867d09d82b0aa93ac52cd2154f19
Saulo Trader <saulo.cervantes@hotmail.com>
2020-12-23 16:23:31 -0600

    14. Ejercicios para la sección

---
+++ 14. Ejercicios para la sección/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Ejercicios_para_la_sección
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
