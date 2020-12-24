b0ea3587ad3315580ce1678cb6c8cf839f40b3dd
Saulo Trader <saulo.cervantes@hotmail.com>
2020-12-24 01:06:38 -0600

    Ejercicio 01

--- 14. Ejercicios para la sección/Program.cs
+++ 14. Ejercicios para la sección/Program.cs
@@ -10,6 +10,19 @@ namespace _14.Ejercicios_para_la_sección
     {
         static void Main(string[] args)
         {
+            /*1-Declara en el lenguaje que prefiera variables o constantes para guardar lo siguientes datos.
+             * Escoja el tipo de dato correcto, que menos espacio ocupen en memoria. 
+             */
+
+            float PrecioCamiseta = 14.99f;
+            byte Edad = 26;
+            const double VelocidadLuz = 2998e+8;
+            string Slogan = "\"Just do it\", \"Impossible is nothing\"";
+            const float Impuesto = 0.7f;
+            long RadioEstrella = 468346864571;
+            var EficienciaEnergética = "A";
+            bool Resultado;
+            const float Gravedad = 9.80665f;
         }
     }
 }
