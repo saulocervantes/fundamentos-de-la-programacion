876e9cbfdb5e98f79ca1f7d2cfec51a44e501486
Saulo Trader <saulo.cervantes@hotmail.com>
2021-01-03 21:22:20 -0600

    Ejercicio 08

--- 22. Ejercicios para la sección/Program.cs
+++ 22. Ejercicios para la sección/Program.cs
@@ -63,46 +63,85 @@ namespace _22.Ejercicios_para_la_sección
              * En caso de elegir > el saldo jamás podría quedar en 0. Es decir que en realidad el saldo disponible sería igual a saldo disponible -1.
              */
 
-            string Producto;
-            string Precio;
-            decimal Saldo = 10000;
-            byte Respuesta;
-            decimal Total = 0;
+            //string Producto;
+            //string Precio;
+            //decimal Saldo = 10000;
+            //byte Respuesta;
+            //decimal Total = 0;
 
-            while (true)
+            //while (true)
+            //{
+            //    Console.WriteLine("Ingrese el nombre del producto:");
+            //    Producto = Console.ReadLine();
+
+            //    Console.WriteLine("Ingrese el precio de {0}", Producto);
+            //    Precio = Console.ReadLine();
+
+            //    if (decimal.Parse(Precio) <= Saldo)
+            //    {
+            //        Console.WriteLine("¿Desea gregar otro producto al carrito  0.-No 1.-Sí");
+            //        Respuesta = byte.Parse(Console.ReadLine());
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Saldo insuficiente.");
+            //        continue;
+            //    }
+
+            //    Total += decimal.Parse(Precio);
+
+            //    if (Respuesta == 0 || Total == Saldo)
+            //    {
+            //        Saldo -= Total;
+            //        Console.WriteLine("Su saldo es {0}, Gracias por su compra.", Saldo);
+            //        Console.ReadLine();
+            //        break;
+            //    }
+            //    else if (Respuesta == 1 )
+            //    {
+            //        continue;
+            //    }
+                
+            //}
+
+            /* 8- En esta ventana podemos ver el listado de gastos. Imaginemos
+             * que tales gastos se encuentran almacenados en una matriz donde
+             * se almacena el concepto, la fecha y la cantidad del gasto.
+             * 
+             * ¿Qué bucle usarías para recorrer toda esa información y mostrarla?
+             * Un bucle for.
+             */
+
+            string[,] Movimientos = new string[5, 3];
+
+            Movimientos[0, 0] = "Disposición de efectivo en cajero.";
+            Movimientos[0, 1] = "18/05/11";
+            Movimientos[0, 2] = "-60";
+
+            Movimientos[1, 0] = "Pago con tarjeta de gasollineras.";
+            Movimientos[1, 1] = "18/05/11";
+            Movimientos[1, 2] = "-56";
+
+            Movimientos[2, 0] = "Recibo de funddaciones y o.n.g.";
+            Movimientos[2, 1] = "18/05/11";
+            Movimientos[2, 2] = "-25";
+
+            Movimientos[3, 0] = "Pago con tarjeta en medicina, farma...";
+            Movimientos[3, 1] = "17/05/11";
+            Movimientos[3, 2] = "-45.31";
+
+            Movimientos[4, 0] = "Pago con tarjeta en gasolineras.";
+            Movimientos[4, 1] = "17/05/11";
+            Movimientos[4, 2] = "-60";
+
+            for (int i = 0; i < Movimientos.GetLength(0); i++)
             {
-                Console.WriteLine("Ingrese el nombre del producto:");
-                Producto = Console.ReadLine();
-
-                Console.WriteLine("Ingrese el precio de {0}", Producto);
-                Precio = Console.ReadLine();
-
-                if (decimal.Parse(Precio) <= Saldo)
+                for (int j = 0; j < Movimientos.GetLength(1); j++)
                 {
-                    Console.WriteLine("¿Desea gregar otro producto al carrito  0.-No 1.-Sí");
-                    Respuesta = byte.Parse(Console.ReadLine());
+                    Console.WriteLine("{0}", Movimientos[i, j]);
                 }
-                else
-                {
-                    Console.WriteLine("Saldo insuficiente.");
-                    continue;
-                }
-
-                Total += decimal.Parse(Precio);
-
-                if (Respuesta == 0 || Total == Saldo)
-                {
-                    Saldo -= Total;
-                    Console.WriteLine("Su saldo es {0}, Gracias por su compra.", Saldo);
-                    Console.ReadLine();
-                    break;
-                }
-                else if (Respuesta == 1 )
-                {
-                    continue;
-                }
-
             }
+            Console.ReadLine();
 
         }
     }
