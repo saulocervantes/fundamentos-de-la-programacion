832017e06186027c1a1777724f1219c68aee6297
Saulo Trader <saulo.cervantes@hotmail.com>
2021-01-03 18:17:57 -0600

    Ejercicio 01, 02, 03

---
+++ 22. Ejercicios para la sección/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Ejercicios_para_la_sección
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Para establecer el Pin de Seguridad, se pide al usuario que lo introduzca 2 veces
+             * hasta que ambas veces coincida.
+             * 
+             * 1- ¿Qué bucle y variables usarías para hacer la comprobación después que el
+             * usuario introduzca 2 veces el pin?
+             */
+
+            string Pin = null;
+            string Comprobacion = null;
+
+            while (true)
+            {
+                Console.WriteLine("Ingrese el Pin de seguridad");
+                Pin = Console.ReadLine();
+
+                Console.WriteLine("Ingrese nuevamente el Pin de seguridad");
+                Comprobacion = Console.ReadLine();
+
+                if (Pin == Comprobacion)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("El Pin no coincide, favor de intentar de nuevo.");
+                }
+            }
+            
+            Console.WriteLine("El Pin ha sido establecido.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 22. Ejercicios para la sección/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("22. Ejercicios para la sección")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("22. Ejercicios para la sección")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("09fe0738-0de5-4f1a-ad1e-475581e2ebbb")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
