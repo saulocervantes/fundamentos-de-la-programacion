f02009638c5b2f3ed3b6734c26783254412a5d9a
Saulo Trader <saulo.cervantes@hotmail.com>
2021-01-23 01:37:20 -0600

    Ejercicio 02

--- 28. Ejercicios para la sección/Program.cs
+++ 28. Ejercicios para la sección/Program.cs
@@ -17,14 +17,35 @@ namespace _28.Ejercicios_para_la_sección
          * Esta función reproducirá el ejercicio solo una vez. ¿Cómo haremos para recrear la serie completa?
          * R = Agregando como parámetro el número de repeticiones y un ciclo.
          */
+
+        /*
+         * 2- Crearemos otra función en la que administraremos cada serie, indicándole
+         * el número de veces que se tiene que realizar el movimiento y el número de
+         * segundos de descanso que corresponde a tal serie.
+         */
         static void Main(string[] args)
         {
-
+            reproducirEjercicio(5, "sentadilla", 5);
         }
 
-        public void reproducirEjercicio(string NombreEjercicio)
+        public static void reproducirEjercicio(string NombreEjercicio)
         {
-            Console.WriteLine("Reproduciendo ejercicio {0}",NombreEjercicio);
+            Console.WriteLine("Reproduciendo ejercicio {0}", NombreEjercicio);
+        }
+
+        public static void reproducirEjercicio(byte Repeticiones, string NombreEjercicio, byte Descanso = 0)
+        {
+
+            for (int i = 1; i <= Repeticiones; i++)
+            {
+                Console.WriteLine("Reproduciendo ejercicio {0} serie: {1}", NombreEjercicio, i);
+
+                if (i == Repeticiones)// Si esta es es la última repetición no se reproduce el descanso.
+                    break;
+                Console.WriteLine("Reproduciendo descanso de {0} segundos", Descanso);
+            }
+            Console.Read();
+
         }
     }
 }
